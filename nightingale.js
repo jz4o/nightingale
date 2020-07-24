8be5c5bb07177da287b4f622beddaf8a1947e22e
jz4o <26317360+jz4o@users.noreply.github.com>
2020-07-24 22:51:18 +0900

    implemented join to channel

--- nightingale.js
+++ nightingale.js
@@ -5,3 +5,22 @@ const client = new Discord.Client();
 
 client.login(config.discord.botToken);
 
+client.on('ready', () => {
+  client.joinToChannel(config.discord.voiceChannelName);
+});
+
+client.joinToChannel = async channelName => {
+  const channel = client.channels.cache.find(channel => {
+    return channel.name === channelName;
+  });
+
+  if (!channel) {
+    console.log('not found channel...');
+
+    client.destroy();
+    return;
+  }
+
+  return await channel.join();
+}
+
