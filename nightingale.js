fd0e5ce85289225f7cfed7bc8002b74faff1ce90
jz4o <26317360+jz4o@users.noreply.github.com>
2020-07-24 23:49:51 +0900

    implemented nightingale sings

--- nightingale.js
+++ nightingale.js
@@ -3,10 +3,35 @@ const config = require('config');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const ytdl = require('ytdl-core');
+
+const Nightingale = class {
+  static discordConnection;
+
+  static setDiscordConnection = connection => {
+    this.discordConnection = connection;
+  }
+
+  static sing = async videoUrl => {
+    const audioStream = await ytdl(videoUrl, { filter: 'audioonly' });
+    this.discordConnection.play(audioStream);
+  }
+}
+
 client.login(config.discord.botToken);
 
-client.on('ready', () => {
-  client.joinToChannel(config.discord.voiceChannelName);
+client.on('ready', async () => {
+  const connection = await client.joinToChannel(config.discord.voiceChannelName);
+
+  Nightingale.setDiscordConnection(connection);
+});
+
+client.on('message', message => {
+  if (!message.content.startsWith(config.discord.videoUrlPrefix)) {
+    return;
+  }
+
+  Nightingale.sing(message.content);
 });
 
 client.joinToChannel = async channelName => {
