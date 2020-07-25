2b27516fbf6fdd107c7ec69e10d48d822797fca2
jz4o <26317360+jz4o@users.noreply.github.com>
2020-07-25 11:10:14 +0900

    fix to nightingale sings when resting only

--- nightingale.js
+++ nightingale.js
@@ -6,7 +6,18 @@ const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 
 const Nightingale = class {
+  static PLAY_STATUSES = {
+    RESTING: 0,
+    SINGING: 1
+  };
+
+  static playStatus = this.PLAY_STATUSES.RESTING;
+
   static discordConnection;
+
+  static isResting = () => {
+    return this.playStatus === this.PLAY_STATUSES.RESTING;
+  }
 
   static setDiscordConnection = connection => {
     this.discordConnection = connection;
@@ -14,7 +25,13 @@ const Nightingale = class {
 
   static sing = async videoUrl => {
     const audioStream = await ytdl(videoUrl, { filter: 'audioonly' });
-    this.discordConnection.play(audioStream);
+    const dispatcher = this.discordConnection.play(audioStream);
+
+    this.playStatus = this.PLAY_STATUSES.SINGING;
+
+    dispatcher.once('finish', () => {
+      this.playStatus = this.PLAY_STATUSES.RESTING;
+    });
   }
 }
 
@@ -31,7 +48,9 @@ client.on('message', message => {
     return;
   }
 
-  Nightingale.sing(message.content);
+  if (Nightingale.isResting()) {
+    Nightingale.sing(message.content);
+  }
 });
 
 client.joinToChannel = async channelName => {
